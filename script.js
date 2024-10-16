d5844a830e1417c44573aac0fc6bab2676ff1687
Vilislav <vilislavangelov@gmail.com>
2024-10-16 07:24:55 +0300

    Tested with multiple different inputs and leetcode
    It all works now and I might come back to refactor
    and make it execute faster and clean up the code.

--- script.js
+++ script.js
@@ -6,18 +6,45 @@ var romanToInt = function(s) {
     for (let i = 0; i <= s.length; i++) {
         switch (sArr[i]) {
             case 'I':
+                if(sArr[i+1] == 'V'){
+                    result += 4;
+                    i++;
+                    break;
+                } else if (sArr[i+1] == 'X') {
+                    result += 9;
+                    i++;
+                    break;
+                }
                 result += 1;
                 break;
             case 'V':
                 result += 5;
                 break;
             case 'X':
+                if(sArr[i+1] == 'L'){
+                    result += 40;
+                    i++;
+                    break;
+                } else if (sArr[i+1] == 'C') {
+                    result += 90;
+                    i++;
+                    break;
+                }
                 result += 10;
                 break;
             case 'L':
                 result += 50;
                 break;
             case 'C':
+                if(sArr[i+1] == 'D'){
+                    result += 400;
+                    i++;
+                    break;
+                } else if (sArr[i+1] == 'M') {
+                    result += 900;
+                    i++;
+                    break;
+                }
                 result += 100;
                 break;
             case 'D':
